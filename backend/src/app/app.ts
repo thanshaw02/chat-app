6612ab6732710992d3646d513d96381956b46674
Tylor Hanshaw <tylorjhanshaw@gmail.com>
2023-03-23 16:33:09 -0400

    added nodemon and socket.io -- changed file structure

---
+++ backend/src/app/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { 
+  Request, 
+  Response 
+} from "express"
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world!");
+});
+
+export default app;
---
+++ backend/src/index.ts
@@ -0,0 +1,5 @@
+import app from "./app/app";
+
+app.listen(8000, () => {
+  return console.log('Express is listening at "http://localhost:8000"');
+});
